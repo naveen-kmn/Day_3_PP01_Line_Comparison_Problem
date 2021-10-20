d10bb7512724f0cd550268b4978fe40787f6afb4
Naveen KM <kmnaveenmech@gmail.com>
2021-10-20 16:43:12 +0530

    Added: UC1 Legth Calculation file is added

--- src/com/blp/line_comparison_problem/LineComparison.java
+++ src/com/blp/line_comparison_problem/LineComparison.java
@@ -1,9 +1,29 @@
 package com.blp.line_comparison_problem;
 
+import java.util.Scanner;
+
 public class LineComparison
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Line Comparison Computation Program");
+            double x1,y1,x2,y2,db;
+
+            Scanner n = new Scanner(System.in);
+
+            System.out.println("Enter the value of x1: ");
+            x1 = n.nextDouble();
+
+            System.out.println("Enter the value of x2: ");
+            x2 = n.nextDouble();
+
+            System.out.println("Enter the value of y1: ");
+            y1 = n.nextDouble();
+
+            System.out.println("Enter the value of y2: ");
+            y2 = n.nextDouble();
+
+            db = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            System.out.println("Distance between two points :" + db);
     }
 }
